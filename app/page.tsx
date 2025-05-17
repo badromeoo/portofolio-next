db902d8e76e23552d7dcaf1d07433bd2f0bdaf3f
I Made Bintang Astawa <astawabintang@gmail.com>
2025-05-17 15:12:32 +0800

    versi 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,76 @@
 import Image from "next/image";
-
+import Lanyard from "./components/Lanyard/Lanyard";
+import RotatingText from "./components/RotatingText/RotatingText";
+import SplitText from "./components/SplitText/SplitText";
+import BlurText from "./components/BlurText/BlurText";
+import Aurora from "./components/Aurora/Aurora";
+import RollingGallery from "./components/RollingGallery/RollingGallery";
+import TrueFocus from "./components/TrueFocus/TrueFocus";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen overflow-x-hidden bg-[#0c0c0c]">
+      <Aurora colorStops={["#00D8FF", "#7CFF67", "#00D8FF"]} blend={0.5} amplitude={1.0} speed={1} />
+      <div className="">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">
+            <div className="flex items-center gap-2">
+              <h1 className="text-2xl font-bold">I'm Ready For Job</h1>
+              <RotatingText
+                texts={["Web Design", "Web Development", "Web Programming"]}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-[#A2EF44] text-black overflow-hidden py-0.5 sm:py-1  justify-center rounded-lg text-2xl font-bold inline-flex"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </div>
+            <div className="flex flex-col items-start mt-4">
+              <SplitText
+                text="I'm Bintang"
+                className="text-6xl font-bold text-[#f3f3f3] "
+                animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+                animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+                threshold={0.1}
+                delay={150}
+              />
+              <SplitText
+                text="Fullstack Developer"
+                className="text-6xl font-bold text-[#A2EF44] "
+                animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+                animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+                threshold={0.1}
+                delay={150}
+              />
+            </div>
+            <div>
+              <BlurText
+                text="I am a highly motivated and results-oriented recent graduate with a Bachelor's degree in Information Systems from ITB STIKOM Bali. I have developed a strong foundation in web development, including proficiency in HTML, CSS, and JavaScript. My practical experience in IT support was gained through an internship at a contracting company, where I honed my technical skills and learned the importance of effective problem-solving.
+              "
+                delay={50}
+                animateBy="words"
+                direction="top"
+                className="text-xl font-bold mb-8"
+              />
+            </div>
+          </div>
+          <div className="col-span-5 mt-0">
+            <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]}></Lanyard>
+          </div>
+          <div className="col-span-12 text-center mt-10">
+            <TrueFocus sentence="My Projects" manualMode={false} blurAmount={5} borderColor="green" animationDuration={2} pauseBetweenAnimations={1} />
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="col-span-12 mt-0">
+            <RollingGallery autoplay={true} />
+
+            <div></div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
